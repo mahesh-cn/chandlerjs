6018953c87af9ca3716f0b8e123eed0b61da13cf
Mahesh <mahesh.cn@outlook.com>
2020-08-29 00:17:51 -0700

    bable and build

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send("<h1>Could I BE anymore usefull</h1>");
+    res.send("<h1>Could I BE more useless</h1>");
 });
 
 app.listen(PORT, () => {
